179220738ae6d3d540f882589b5880e16dfc4f98
LuisAlejandro10 <lportadatello@gmail.com>
2026-05-12 15:48:30 -0600

    Espacio de trabajo

--- src/output/render.cpp
+++ src/output/render.cpp
@@ -1,2 +1,26 @@
 #include "raylib.h"
 
+#include "raylib.h"
+#include <iostream>
+
+
+int main()
+{
+    InitWindow(800, 450, "Mi primer proyecto con raylib y C++");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawText("Hola raylib no se que haccer en mi vida", 190, 200, 20, BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
